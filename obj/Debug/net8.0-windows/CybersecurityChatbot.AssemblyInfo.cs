8066f17b9c4bd9fbd7992baedcae9496c27cb69a
ST10345327 <ST10345327@RCCONNECT.EDU.ZA>
2025-05-21 16:41:56 +0200

    Moved files from nested directory, updated structure for Part 2, excluded interaction_log.txt

--- obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a516ee4ba94a14d9963e916bde6ee475edf37fb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
